9d196282d0865577424603b50f8edf8d599f2c57
Cipriangr <grumazescu_ciprian@yahoo.com>
2021-05-14 17:38:33 +0300

    added filter search + responsive edits

---
+++ index.js
@@ -0,0 +1,37 @@
+const books = [
+  {
+    name: 'Banii - Stapaneste jocul',
+    author: 'Tony Robbins',
+    description: 'O carte care isi propune sa ghideze cititorul spre obtinerea libertatii si sigurantei financiare prin 7 pasi simpli.',
+    coverUrl: 'https://cdn.dc5.ro/img-prod/1048325-1.png',
+    price: '68 RON'
+  },
+  {
+    name: 'De la zero la unu',
+    author: 'Blake Masters, Peter Thiel',
+    description: 'Daca vrei sa construiesti un viitor mai bun, trebuie sa crezi in secrete.',
+    coverUrl: 'http://mcdn.elefant.ro/mnresize/1500/1500/images/25/247925/de-la-zero-la-unu-note-despre-startupuri-sau-cum-sa-construiesti-viitorul_1_fullsize.jpg',
+    price: '37 RON'
+  },
+  {
+    name: 'Incepe cu de ce',
+    author: 'Simon Sinek',
+    description: 'Cum ne inspira marii lideri astfel incat sa trecem la actiune',
+    coverUrl: 'http://mcdn.elefant.ro/mnresize/350/350/images/87/1172987/incepe-cu-de-ce-cum-ne-inspira-marii-lideri-astfel-incat-sa-trecem-la-actiune_1_fullsize.jpg',
+    price: '49 RON'
+  },
+  {
+    name: 'Cele 7 deprinderi ale persoanelor eficace',
+    author: 'Stephen R. Covey',
+    description: 'Cele 7 deprinderi formulate in urma a zeci de ani de experienta si cercetare ii deosebesc pe cei fericiti, impliniti si sanatosi de cei care sunt supusi esecului.',
+    coverUrl: 'http://mcdn.elefant.ro/mnresize/350/350/images/25/360125/cele-7-deprinderi-ale-persoanelor-eficace_1_fullsize.jpg',
+    price: '55 RON'
+  },
+  {
+    name: 'Startup de 100$',
+    author: 'Chris Guillebeau',
+    description: 'Chris a descoperit 1.500 de indivizi asemenea lui care si-au construit afaceri cu venituri de peste 50.000$ anual pornind de la o investitie modesta (in multe cazuri 100$ sau mai putin)',
+    coverUrl: 'http://mcdn.elefant.ro/mnresize/350/350/images/56/207256/startup-de-100_1_fullsize.jpg',
+    price: '47 RON'
+  }
+];
--- js/main.js
+++ js/main.js
@@ -1,23 +1,126 @@
-
 function openLogin(){
   document.getElementById("loginform").style.display = "flex";
   
-}
+}  
 
 function closeLogin(e){
   document.getElementById("loginform").style.display = "none";
+  
+}  
 
-}
+
+$('.search-toggle').on('click', function(){
+  $('.search-field').toggleClass('show-search')
+})  
+
+$('.search-mobile-btn').on('click', function(){
+  $('.search-field-medium').toggleClass('show-search')
+})  
+
+
 
 document.onkeydown = function(evt) {
   evt = evt || window.event;
   if (evt.keyCode == 27) {
     document.getElementById("loginform").style.display = "none";
-  }
+  }  
 
+};  
+
+const cards = [
+  {
+    image: './img/layer1.png',
+    title: 'Statistics table',
+    price: '$759.35'
+  },
+  {
+    image: './img/layer2.png',
+    title: 'Pen',
+    price: '$759.55'
+  },
+  {
+    image: './img/layer3.png',
+    title: 'Keyboard',
+    price: '$129.55'
+  },
+  {
+    image: './img/layer4.png',
+    title: 'Clock',
+    price: '$299.99'
+  },
+  {
+    image: './img/layer5.png',
+    title: 'Interactive Display',
+    price: '$600.95'
+  },
+  {
+    image: './img/layer6.png',
+    title: 'Tory Desk Calendar',
+    price: '$330.99'
+  },
+  {
+    image: './img/layer7.png',
+    title: 'Something nice',
+    price: '$759.55'
+  },
+  {
+    image: './img/layer6.png',
+    title: 'Something cool too',
+    price: '$900.22'
+  }
+]
+
+
+
+const cardsContent = document.querySelector('#cards-content');
+const searchInput = document.querySelector('#custom-search');
+
+
+function renderCards(data){
+  let cardElements = '';
+  if(data && data.length>0){
+    data.forEach(card =>{
+      const html = `
+      <div class="col-md-3 mb-4">
+        <div class="card mb-3 h-100">
+          <div class="hover">
+              <span class="addtocart">
+                <img class="cart-item" src="img/plus.png" alt="#">
+              </span>
+              <span class="addtowishlist">
+                <img src="img/heartred.png" alt="#">
+              </span>
+          </div>
+          <img src="${card.image}" class="card-img-top" alt="#">
+          <div class="card-body">
+            <h4 class="card-title">${card.title}</h4>
+            <p class="card-text">${card.price}</p> 
+          </div>
+        </div>
+      </div>
+      `;
+      cardElements += html;
+    })
+  }
+  cardsContent.innerHTML = cardElements
 };
 
+renderCards(cards)
+
+let filteredCards = []; 
+
+searchInput.addEventListener('keyup', (e)=>{
+  if(e.target.value.length >1){
+    filteredCards = cards.filter( card => card.title.toLowerCase().includes(e.target.value.toLowerCase()) || card.price.toLowerCase().includes(e.target.value.toLowerCase()));
+    renderCards(filteredCards)
+  }else{
+    renderCards(cards)
+  }
+});
+
+
 (function(){
+
   const wishBtn = document.querySelectorAll('.addtowishlist');
   wishBtn.forEach(function(btn){
     btn.addEventListener('click', function(event){
@@ -31,7 +134,7 @@ document.onkeydown = function(evt) {
         let name = event.target.parentElement.parentElement.nextElementSibling.nextElementSibling.children[0].textContent;
         wish.name= name;
         
-        const wishItem = document.createElement("li");
+        const wishItem = document.createElement("li");  
         wishItem.classList.add('wish-item');
         wishItem.innerHTML = `<span class="wish-product-img"><img src="${wish.img}" alt=""></span>
         <div class="item-content"
@@ -48,21 +151,21 @@ document.onkeydown = function(evt) {
         wishItem.querySelector('.wish-item-remove').addEventListener('click', (e) =>{
           const theLI = e.target.parentNode.parentNode
           theLI.remove()
-        })
+        })  
 
 
           if(wishcart.children.length >=4){
             noitem.style.display = 'none';
-          }
-        // const wishcontent = document.getElementById('wish-area');
+          }  
+        // const wishcontent = document.getElementById('wish-area');  
         // const wishcart = document.getElementById('wishlist-content');
         // if(wishcart.children.length <=2){
-        //   wishcontent.style.display = 'none' ;
+        //   wishcontent.style.display = 'none' ;  
         //   console.log(wishcart.children.length)
         // }
-      }
-    })
-  })
+      }  
+    })  
+  })  
   
   
   const cardBtn= document.querySelectorAll('.addtocart');
@@ -102,23 +205,23 @@ document.onkeydown = function(evt) {
           noitemCart.style.display = "none"
           info.style.display = "flex";
           
-        }
+        }  
         if(cart.children.length <3){
           console.log(info)
         
-        }
+        }  
         cart.insertBefore(cartItem, total);
         //remove item on click trash icon
         cartItem.querySelector(".cart-item-remove").addEventListener("click", (e) => {
           const theLI = e.target.parentNode.parentNode;
           theLI.parentNode.removeChild(theLI);
           
-        });
+        });  
         
         showTotals();
-      }
-    });
-  });
+      }  
+    });  
+  });  
   
   
   function deletelement(){
@@ -126,7 +229,7 @@ document.onkeydown = function(evt) {
     const finalremove = removelist.remove()
     console.log(removelist);
     
-  }
+  }  
   
   
   
@@ -136,16 +239,16 @@ document.onkeydown = function(evt) {
     const items = document.querySelectorAll('.cart-item-price');
     items.forEach(function(item){
       totalPrice.push(parseFloat(item.textContent));
-    })
+    })  
     const totalMoney = totalPrice.reduce(function(totalPrice,item){
       totalPrice +=item;
       
       return totalPrice;
-    }, 0)
+    }, 0)  
     const FinalMoney = totalMoney.toFixed(2)
     document.getElementById('total-price').textContent = FinalMoney;
     document.getElementById('items-number').textContent = totalPrice.length;
-  }
+  }  
   
   
   
@@ -155,25 +258,25 @@ document.onkeydown = function(evt) {
     const totalPrice = [];
     items.forEach(function(item){
       totalPrice.push(parseFloat(item.textContent));
-    })
+    })  
     const totalMoney = totalPrice.reduce(function(totalPrice,item){
       totalPrice +=item;
       
       return totalPrice;
-    }, 0)
+    }, 0)  
     const FinalMoney = totalMoney.toFixed(2)
     document.getElementById('total-price').textContent = FinalMoney;
     document.getElementById('items-number').textContent = totalPrice.length;
-  }
+  }  
   
   
   //delete items
   
   function deleteItems(){
     
-  }
+  }  
   
-})();
+})();  
 
 $(".toggle-password").click(function() {
   
@@ -183,8 +286,8 @@ $(".toggle-password").click(function() {
     input.attr("type", "text");
   } else {
     input.attr("type", "password");
-  }
-});
+  }  
+});  
 
 
 
@@ -192,9 +295,9 @@ $(".toggle-password").click(function() {
 // x = 10;
 // function test()
 // {
-//     var x;
+//     var x;  
 //     if (x > 20) {
-//         x = 50;
+//         x = 50;  
 //     }
 
 //     console.log(x);
@@ -204,15 +307,15 @@ $(".toggle-password").click(function() {
 
 // var a = 10;
 // (function() {
-//   var a = 15;
+//   var a = 15;  
 //   window.x = function() {
-//     alert(a);
+//     alert(a);  
 //   }
 // })();
 // x();
 
 // var test1 = {
-//   name: 'test',
+//   name: 'test',  
 //   age: '12'
 // }
 // var test2 = test1;
@@ -220,7 +323,80 @@ $(".toggle-password").click(function() {
 
 
 // if(test2 == test1){
-//   console.log(test2)
+//   console.log(test2)  
 // }else{
-//   console.log('eroare')
+//   console.log('eroare')  
 // }
+// Setup
+// Setup
+
+
+
+// function phoneticLookup(val) {
+//   var result = "";  
+
+//   // Only change code below this line
+//   var lookup = {
+//       "alpha": "Adams",  
+//       "bravo": "Boston",
+//       "charlie":"Chicago",
+//       "delta": "Denver",
+//       "echo":"Easy",
+//       "foxtrot":"Frank"
+//   }
+//   result = lookup[val]
+//   // Only change code above this line
+//   return result;
+// }
+
+// console.log(phoneticLookup("charlie"));
+
+
+//object exercise
+// var contacts = [
+//   {
+//       "firstName": "Akira",  
+//       "lastName": "Laine",
+//       "number": "0543236543",
+//       "likes": ["Pizza", "Coding", "Brownie Points"]
+//   },
+//   {
+//       "firstName": "Harry",  
+//       "lastName": "Potter",
+//       "number": "0994372684",
+//       "likes": ["Hogwarts", "Magic", "Hagrid"]
+//   },
+//   {
+//       "firstName": "Sherlock",  
+//       "lastName": "Holmes",
+//       "number": "0487345643",
+//       "likes": ["Intriguing Cases", "Violin"]
+//   },
+//   {
+//       "firstName": "Kristian",  
+//       "lastName": "Vos",
+//       "number": "unknown",
+//       "likes": ["JavaScript", "Gaming", "Foxes"]
+//   }
+// ];
+
+
+// function lookUpProfile(name, prop) {
+//   for (let x = 0; x < contacts.length; x++) {
+//     if (contacts[x].firstName === name) {
+//       if (contacts[x].hasOwnProperty(prop)) {
+//         return contacts[x][prop];  
+//       }
+//       else{
+//         return "No such property";  
+//       }
+//     }else{
+//       return "No such contact";  
+//     }
+//   }
+// }
+
+// lookUpProfile("Akira");
+
+// lookUpProfile("Akira", "likes");
+
